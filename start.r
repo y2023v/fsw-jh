1ee184c347e4f9e2ee46e5cb324014d2f1dc3559
y2023v <122984920+y2023v@users.noreply.github.com>
2023-01-22 18:25:31 +0100

    Create start.r

---
+++ start.r
@@ -0,0 +1,7 @@
+# Gegeven is de reeks getallen 7,2, 11, 9, 10, 10, 7, 5, 9, 9, 9, 5 
+
+# Oef 1a: Bewaar deze reeks in een vector met naam vect_getallen
+
+# Oef 1b: Bereken het gemiddelde van deze reeks getallen
+
+# Oef 1c: Maak een histogram van de data
